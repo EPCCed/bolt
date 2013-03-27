5e979ead45008f30a74c2fa2b83d11eea20bcf4c
Andrew Turner <a.turner@epcc.ed.ac.uk>
2013-03-27 20:59:29 +0000

    Added test configuration files and started writing task distribution test

--- unittest/testJob.py
+++ unittest/testJob.py
@@ -1,16 +1,36 @@
 import unittest
+import os
 from job import Job
+from batch import Batch
+from resource import Resource
+from code import Code
 
 class JobTestCase(unittest.TestCase):
+    
+    configDir = "/unittest/configuration"
+    batchConfig = "test.batch"
+    resourceConfig = "test.resource"
+    codeConfig = "test.code"
+
     def setUp(self):
         self.job = Job()
+
     def testName(self):
         self.job.setName("test")
         assert self.job.name == "test", "Job name not set correctly = '{0}' (should be '{1}').".format(self.job.name, "test")
+
     def testParallel(self):
         self.job.setIsParallel(True)
         assert self.job.isParallel, "Job is not parallel and should be."
-        
+
+    # Test the distribution of parallel tasks (e.g. pure MPI job)
+    def testParallelTaskDitribution(self):
+        rootDir = os.environ['BOLT_DIR']
+        batch = Batch()
+        batch.readConfig(rootDir + configDir + "/" + batchConfig)
+        resource = Resource()
+        resource.readConfig(rootDir + configDir + "/" + resourceConfig)
+              
         # To test the functionality of the task distribution we need a resource configured. We can do this by re
         # reading a resource configuration file. We probably need to place a resource configuration file
         # in the test directory to make this work. We would also need a batch configuration file???
